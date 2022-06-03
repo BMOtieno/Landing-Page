1615bf55fede52612c640ee05dfa24da1f537b01
BrianOtieno <48413981+BMOtieno@users.noreply.github.com>
2022-06-02 23:20:41 -0400

    Added Active Class in Sections

--- js/app.js
+++ js/app.js
@@ -62,13 +62,40 @@ for(let i = 1; i <= 4; i++){
 
 
 // Scroll to anchor ID using scrollTO event
-
+const secOne = document.getElementById("section1");
+const secTwo = document.getElementById("section2");
+const secThree = document.getElementById("section3");
+const secFour = document.getElementById("section4");
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
+window.addEventListener('scroll', () =>{
+
+    const scrolled = window.scrollY;
+    
+    switch(scrolled){
+        case 300:
+            secOne.classList.add('active');
+            break;
+        case 1400:
+            secOne.classList.remove('active');
+            secTwo.classList.add('active');
+            break;
+        case 2500:
+            secTwo.classList.remove('active');
+            secThree.classList.add('active');
+            break;
+        case 3800:
+            secThree.classList.remove('active');
+            secFour.classList.add('active');
+            break;
+    }
+
+    
+});
 
 // Build menu 
 
