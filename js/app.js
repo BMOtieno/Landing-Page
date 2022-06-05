ba174ef60e403a6e85f873d9de9b391c6218be8f
BrianOtieno <48413981+BMOtieno@users.noreply.github.com>
2022-06-05 13:17:09 -0400

    Init

--- js/app.js
+++ js/app.js
@@ -36,14 +36,14 @@ const allSect = document.querySelectorAll("section");
 
 // Build menu 
 // build the nav
+
 for(let i = 1; i <= allSect.length; i++){
     const myLi = document.createElement("li"); 
     myLi.innerHTML = "Section " + i;
-    myLi.style.color = "black";
     myUl.appendChild(myLi);   
     // Scroll to section on link click
-    myLi.addEventListener('click', function(){
-        document.location.href = '#section' + i;
+    myLi.addEventListener("click", () => {
+        document.location.href = "#section" + i;
     });
 }
 
@@ -51,14 +51,14 @@ const onScroll = () => {
     //numerical value when user scrolls through the page
     const scrollValue = document.documentElement.scrollTop;
     // Set sections as active
-    (scrollValue > 240 && scrollValue < 900) ? secOne.classList.add('your-active-class') : secOne.classList.remove('your-active-class');
-    (scrollValue > 900 && scrollValue < 1710) ? secTwo.classList.add('your-active-class') : secTwo.classList.remove('your-active-class');
-    (scrollValue > 1710 && scrollValue < 2800) ? secThree.classList.add('your-active-class'): secThree.classList.remove('your-active-class');
-    (scrollValue > 2800 && scrollValue < 4260) ? secFour.classList.add('your-active-class') : secFour.classList.remove('your-active-class');
+    (scrollValue > 240 && scrollValue < 900) ? secOne.classList.add("your-active-class") : secOne.classList.remove("your-active-class");
+    (scrollValue > 900 && scrollValue < 1710) ? secTwo.classList.add("your-active-class") : secTwo.classList.remove("your-active-class");
+    (scrollValue > 1710 && scrollValue < 2800) ? secThree.classList.add("your-active-class"): secThree.classList.remove("your-active-class");
+    (scrollValue > 2800 && scrollValue < 4260) ? secFour.classList.add("your-active-class") : secFour.classList.remove("your-active-class");
 }
 
 // Scroll to anchor ID using scrollTO event
-window.addEventListener('scroll', onScroll);
+window.addEventListener("scroll", onScroll);
 
 
 
